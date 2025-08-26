1f0dc39ae9fdee19e73bb56d3fede88dd37f2d70
joshpollara <75546+joshpollara@users.noreply.github.com>
2025-08-26 20:28:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,29 @@
-resource "null_resource" "foo" {
-  provisioner "local-exec" {
-    command = "echo foo"
+terraform {
+  backend "gcs" {
+    bucket = "terrateam-demo-state"
+    prefix = "/infrastructure/staging/compute/tf.state"
+  }
+}
+
+provider "google" {
+  project = "terrateam-sandbox"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "staging" {
+  count        = 1
+  name         = "staging-instance"
+  machine_type = "e2-small"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
   }
 }
